449b6425e6039cf62ef92fe2386fa2ab556a60bb
Cooke <tc2779@pathology.columbia.edu>
2023-08-03 16:24:20 -0400

    Updated clean_csv() fcn

--- AnalysisCode_Trial.R
+++ AnalysisCode_Trial.R
@@ -1,4 +1,27 @@
 ### Test code for consolidating DA2 Na, Cl, CO2 output,
 ### performing anion gap calculation, and making some simple graphs
+library(tidyverse)
 
-read.csv()
+
+setwd(paste0(getwd(),"/../00. Data"))
+
+sodium.csv <- read.csv("SodiumLvl_070323-070323.csv")
+chloride.csv <- read.csv("ChlorideLvl_070323-070323.csv")
+bicarb.csv <- read.csv("CO2_070323-070323.csv")
+
+clean_csv <- function(csv, test){
+  csv <- csv[c(
+    "PERFORM_DT_TM",
+    "X_ACC",
+    "DRAWN_DT_TM",
+    "RECEIVED_DT_TM",
+    "X_RESULT",
+    "X_NORM",
+    "X_CAT"
+  )]
+  names(csv)[which(names(csv) == "X_RESULT")] <- test
+  return(csv)
+}
+
+glimpse(sodium.csv)
+glimpse(chloride.csv)
