e4e5c8d770fd3370c3593e003b9e651289b3c463
Tyler Cooke <tyler.s.cooke@gmail.com>
2023-08-29 20:27:41 -0400

    working version

--- csv output to single dataframe.R
+++ csv output to single dataframe.R
@@ -1,14 +1,30 @@
 # Code to concatentate week-long csv reports into a single data frame
 
-# UPDATE TARGET FILE PATHWAY 'folderpath' BEFORE RUNNING
-
+# UPDATE TARGET FOLDER 'targetFolder' BEFORE RUNNING
+library(dplyr)
 setwd(paste0(getwd(),"/../00. Data"))
 list.files()
 
-folderpath <- paste0(getwd(), '/061123-081923')
+targetFolder <- '061222-082022'
 
-csv.to.dataframe <- function(folderpath) {
+csv.to.dataframe <- function(targetFolder) {
   
+  setwd(paste0(getwd(), '/', targetFolder))
   
-  return(list.files(folderpath))
+  numFiles <- length(list.files())
+  
+  combinedCsv <- read.csv(list.files()[1])
+  
+  for (i in 1:numFiles-1) {
+    combinedCsv <- bind_rows(
+      combinedCsv, 
+      read.csv(
+        paste0(getwd(),'/',list.files()[i+1])
+      )
+    )
+  }
+  
+  write.csv(combinedCsv, file = "combined data.csv", row.names = FALSE)
+  setwd('..')
 }
+
