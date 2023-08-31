a86911d90be5405f86540bb3c04818fc58486251
Tyler Cooke <tyler.s.cooke@gmail.com>
2023-08-30 21:45:30 -0400

    Updated data analysis code

--- MixedModel.R
+++ MixedModel.R
@@ -1,18 +1,49 @@
 # Code to perform mixed model regression on Anion Gap data
+library(tidyverse)
+library(lme4)
 
 # 1) Read in csv's
 setwd(paste0(getwd(), "/.."))
-csv.2023 <- read.csv(paste0(
+csv2023 <- read.csv(paste0(
   getwd(), "/00. Data/02. Combined Data/AGAP_0611-081923.csv"
 ))
-csv.2022 <- read.csv(paste0(
+csv2022 <- read.csv(paste0(
   getwd(), "/00. Data/02. Combined Data/AGAP_0612-082022.csv"
 ))
 
-# 2) THis is still proof of concept, so data doesn't have to be super clean
+csvCombined <- bind_rows(csv2022, csv2023)
+
+# 2) Data cleaning
+    # NEED to remove all SVCRSC values that aren't UC*C702
+    # Recode ENCTYP as dummy variable for 'Emergency', 'Inpatient', 'Outpatient',
+      # and 'Private Ambulatory'
+    # Figure out why there are extra duplicated rows in the data set
+
+clean.csv <- function(dataframe) {
+  
+  names(dataframe) <- str_replace_all(names(dataframe), "X_", "")
+  
+  dataframe$RESULT <- str_trim(dataframe$RESULT, side = "left") %>% 
+    as.numeric()
+  
+  return(dataframe)
+}
+
+df <- clean.csv(csvCombined)
+
+df <- df %>% 
+  mutate(
+    prePost = if_else(str_detect(PERFORM_DT_TM,"/22 "), 0, 1)
+  )
+
 # 3) Start with simple linear regression, then add layers one at a time to make 
     # that I'm doing things correctly
 
+lm.df <- lm(RESULT ~ prePost, data = df)
+summary(lm.df)
+
+lmer.df <- lmer(RESULT ~ prePost + (1|MRN), data = df)
+summary(lmer.df)
 
 
 # 4) Once I have even a couple terms working, can probably reach out to Eldad 
