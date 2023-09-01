d644e04e665b9a42f46f50bc0092485da1a229bb
Tyler Cooke <tyler.s.cooke@gmail.com>
2023-08-31 21:06:38 -0400

    Fixed bug that was duplicating first df

--- csv output to single dataframe.R
+++ csv output to single dataframe.R
@@ -6,26 +6,25 @@ library(dplyr)
 setwd(paste0(getwd(),"/../00. Data/01. Raw Data"))
 list.files()
 
-targetFolder <- '061222-082022'
+targetFolder <- '061123-081923'
 
 csv.to.dataframe <- function(targetFolder) {
-  
+
   setwd(paste0(getwd(), '/', targetFolder))
-  
+
   numFiles <- length(list.files())
-  
+
   combinedCsv <- read.csv(list.files()[1])
-  
-  for (i in 1:numFiles-1) {
+
+  for (i in 1:(numFiles-1)) {
     combinedCsv <- bind_rows(
-      combinedCsv, 
+      combinedCsv,
       read.csv(
         paste0(getwd(),'/',list.files()[i+1])
       )
     )
   }
-  
+
   write.csv(combinedCsv, file = "combined data.csv", row.names = FALSE)
   setwd('..')
 }
-
