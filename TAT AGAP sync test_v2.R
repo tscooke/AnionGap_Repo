0dce559471ce0ff435e955c36d3e7ef00f574314
Cooke <tc2779@pathology.columbia.edu>
2023-11-14 18:24:44 -0500

    Checking for TAT database compatibility with anion gap df - currently 1816 accession numbers in df.2022 are not in TAT

--- TAT AGAP sync test_v2.R
+++ TAT AGAP sync test_v2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # CONCLUSION: 
-# COL - CCL - Laboratory Turn Around Time Report  
+# COL - DA2 - RMC Order Volume Columbia is the CORRECT report, per initial testing  
 # 
 
 setwd(paste0(getwd(), "/.."))
@@ -9,13 +9,13 @@ csv2022 <- read.csv(paste0(
   getwd(), "/00. Data/02. Combined Data/AGAP_0612-082022.csv"
 ))
 tat2022 <- read.csv(paste0(
-  getwd(), "/00. Data/00. Test Data/TAT Columbia_0630-070522.csv"
+  getwd(), "/00. Data/02. Combined Data/TAT_0612-082022.csv"
 ))
 tat2022.stroke <- read.csv(paste0(
   getwd(), "/00. Data/00. Test Data/TAT ColumbiaStroke_0630-070522.csv"
 ))
 
-
+### CLEAN ANION GAP DATA, REMOVE DUPLICATES
 
 clean.csv <- function(dataframe) {
   names(dataframe) <- str_replace_all(names(dataframe), "X_", "")
@@ -24,6 +24,7 @@ clean.csv <- function(dataframe) {
   dataframe <- dataframe %>% 
     filter(!is.na(RESULT)) %>% 
     filter(str_ends(SVCRSC, "C702")) %>% 
+    filter(!(CAT == ".BMPP" | CAT == ".CMPP")) %>% 
     filter(ENCTYP == 'Inpatient' |
              ENCTYP == 'Outpatient' |
              ENCTYP == 'Emergency' |
@@ -43,7 +44,7 @@ clean.csv <- function(dataframe) {
   return(dataframe)
 }
 
-df.2022 <- clean.csv(csv2022)
+df.2022_v2 <- clean.csv(csv2022)
 
 days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 dates.2022 <- str_sub(df.2022$PERFORM_DT_TM, start = 1, end = 8)
@@ -62,18 +63,62 @@ df.2022 <- df.2022 %>%
   )
 
 df.2022.rmdup <- df.2022[!duplicated(df.2022$MRN),]
+df.2022_v2.rmdup <- df.2022_v2[!duplicated(df.2022$MRN),]
+
+### CLEAN TAT DATA
 
 clean.tat <- function(dataframe) {
   names(dataframe) <- str_replace_all(names(dataframe), "\\.\\.\\.", "")
   dataframe <- dataframe %>% 
+    filter(
+      Accession.NbrFormatted != "---",
+      Order.Status == "Completed"
+     # ,Order.Procedure == "Basic Metabolic Panel" | Order.Procedure == "Comprehensive Metabolic Panel"
+    ) %>% 
     mutate(
       ACC = str_trim(Accession.NbrFormatted, side = "right")
     )
   return(dataframe)
 }
 
+tat.2022 <- clean.tat(tat2022)
+
+tat.2022.rmdup <- tat.2022[!duplicated(tat.2022$ACC),]
+
+
+test.acc <- c("2-22-222-12524")
+
+tat.2022[which(tat.2022$ACC == test.acc),]
+
+df.2022.rmdup[!(df.2022.rmdup$ACC %in% tat.2022.rmdup$ACC),] %>% group_by(Date) %>% summarize(Count = n())
+df.2022.rmdup[!(df.2022.rmdup$MRN %in% tat.2022.rmdup$AliasPerson.MRN),] %>% group_by(Date) %>% summarize(Count = n())
+df.2022.rmdup[!(df.2022.rmdup$NAME %in% tat.2022.rmdup$Person.NameFull),] %>% group_by(Date) %>% summarize(Count = n())
+
+df.not.tat <- df.2022.rmdup[!(df.2022.rmdup$ACC %in% tat.2022.rmdup$ACC),]
+ 
+### 4524 Outpatient accession numbers are not included in tat.2022 (also 39 inpatient cases, but I care less about that)
+  # What else is different about those? Are they all Private Ambulatory?
+  # They were logged on the machine, but not on the collected time, in lab time, etc. Maybe they came to the lab a different way? were they stats?
+  # ANSWER: CAT for almost all of them is .BMPP or .CMPP ; idk what kind of test that is, 
+
+# After filtering df.2022 to remove .BMPP and .CMPP, accession numbers that are in tat.2022 but not df.2022 
+  # decreased to 1816. An improvement, but still higher than I would like. More investigating to be done.
+
+
+# 1) Remove duplicate accession numbers
+#    Remove canceled tests
 # 2) Confirm that the accession numbers in tat2022 are in df.2022
 # 3) Make note of which site has the right accession numbers so can just pull data from that in the future
+
+
+
+
+
+
+
+
+
+
 
 tat.2022.stroke <- clean.tat(tat2022.stroke)
 
